b88b0050bacce620122f97a6a95463481e6b874d
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-07-01 09:57:53 +0530

    second template

--- service/certificate-generate.js
+++ service/certificate-generate.js
@@ -15,7 +15,15 @@ function build(dataCallback, endCallback, data) {
     doc.fillColor('black').fontSize(50).font(__dirname+'/assets/fonts/Babylonica-Regular.ttf').text(data[3],300,430);
     doc.fillColor('black').fontSize(20).font(__dirname+'/assets/fonts/PlayfairDisplay-Regular.ttf').text(data[4],350,485);
   }
-
+  else if(temp==='gold')
+  {
+    doc.image(__dirname+'/assets/image/temp_2.png',0,0,{width:850});
+    doc.fillColor('#F5D182').fontSize(20).font(__dirname+'/assets/fonts/PlayfairDisplay-Regular.ttf').text(data[1],340,130);
+    doc.fillColor('#F5D182').fontSize(60).font(__dirname+'/assets/fonts/MsMadi-Regular.ttf').text(data[0],240,230);
+    doc.fillColor('white').fontSize(15).font(__dirname+'/assets/fonts/PlayfairDisplay-Regular.ttf').text(data[2],240,300);
+    doc.fillColor('#F5D182').fontSize(40).font(__dirname+'/assets/fonts/Babylonica-Regular.ttf').text(data[3],330,400);
+    doc.fillColor('white').fontSize(20).font(__dirname+'/assets/fonts/PlayfairDisplay-Regular.ttf').text(data[4],345,450);
+  }
   
   doc.end();
 }
